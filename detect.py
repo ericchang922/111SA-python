5563ea4a118b7a83bca8da8fdd9f5fddd70e17b9
lolauren <lolauren910614>
2023-06-03 10:42:20 +0800

    救護車和計算一般車數量功能分開
    當遇到救護車會回傳true
    計算數量僅會回傳數字

--- detect.py
+++ detect.py
@@ -12,7 +12,7 @@ CORS(app)
 #def convey():
 #    return jsonify(carno)
 
-@app.route('/road-veiw', methods=['GET', 'POST'])
+@app.route('/road-view', methods=['POST'])
 def roadVeiw():
     insertValue = request.get_json()
     roadId = insertValue['roadId']
@@ -23,17 +23,17 @@ def whichRoad(roadId):
     cars = []
     # 统计数量
     carno = 0
-    ambulance_classifier = cv2.CascadeClassifier('ambulance.xml')
+    #ambulance_classifier = cv2.CascadeClassifier('ambulance.xml')
     if roadId == 1:
         cap = cv2.VideoCapture('./img/videoooooooo.mp4')
-        x_left = 500
+        x_left = 400
         x_right = 1500
-        min_w = 180
-        min_h = 180
+        min_w = 200
+        min_h = 200
         line_high = 800
         #偏移量
-        offset = 8
-        carno = cal(cap, x_right, x_left, min_w, min_h, line_high, cars, ambulance_classifier, offset)
+        offset = 7
+        carno = cal(cap, x_right, x_left, min_w, min_h, line_high, cars, offset)
     elif roadId == 2:
         cap = cv2.VideoCapture('./img/roadddd.mp4')
         x_left = 1300
@@ -43,7 +43,7 @@ def whichRoad(roadId):
         line_high = 570
         #偏移量
         offset = 3
-        carno = cal(cap, x_right, x_left, min_w, min_h, line_high, cars, ambulance_classifier, offset)
+        carno = cal(cap, x_right, x_left, min_w, min_h, line_high, cars, offset)
     return carno
 # 计算中心点函数
 def center(x, y, w, h):
@@ -55,7 +55,7 @@ def center(x, y, w, h):
 
 
 
-def cal(cap, x_right, x_left, min_w, min_h, line_high, cars, ambulance_classifier, offset):
+def cal(cap, x_right, x_left, min_w, min_h, line_high, cars, offset):
     carno = 0
     # 引入去背景函数
     bgsubmog = cv2.bgsegm.createBackgroundSubtractorMOG()
@@ -106,33 +106,33 @@ def cal(cap, x_right, x_left, min_w, min_h, line_high, cars, ambulance_classifie
                     (x, y) = cpoint
 
                     # 救护车检测
-                    roi = frame[y:y + h, x:x + w]
-                    gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-                    ambulances = ambulance_classifier.detectMultiScale(gray_roi, scaleFactor=1.1, minNeighbors=5)
+                    # roi = frame[y:y + h, x:x + w]
+                    # gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+                    # ambulances = ambulance_classifier.detectMultiScale(gray_roi, scaleFactor=1.1, minNeighbors=5)
 
                     #辨識救護車，但框出來的不是真正的
                     #如果辨識到救護車會直接回傳carno=9999，這樣就能知道是否有緊急狀況
-                    for (ax, ay, aw, ah) in ambulances:
-                        # 計算救護車中心點位置
-                        ax += x
-                        ay += y
-                        if(aw > min_w and ah > min_h):
-                            #要錄模擬影片可以把下三行先槓掉
-                            cv2.rectangle(frame, (ax, ay), (ax + aw, ay + ah), (0, 0, 255), 2)
-                            carno = 9999
-                            return carno
+                    # for (ax, ay, aw, ah) in ambulances:
+                    #     # 計算救護車中心點位置
+                    #     ax += x
+                    #     ay += y
+                    #     if(aw > min_w and ah > min_h):
+                    #         #要錄模擬影片可以把下三行先槓掉
+                    #         cv2.rectangle(frame, (ax, ay), (ax + aw, ay + ah), (0, 0, 255), 2)
+                    #         carno = 9999
+                    #         return carno
 
-                    if(carno != 9999):
-                        for (x, y) in cars:
-                            cal = y - line_high
-                            if (cal < 0):
-                                cal = cal * -1
-                            if (cal < offset and x > x_left and x < x_right):
-                                print("y:" + str(y))
-                                print("x:" + str(x))
-                                carno += 1
-                                cars.remove((x, y))
-                                print(carno)
+                    # if(carno != 9999):
+                    for (x, y) in cars:
+                        cal = y - line_high
+                        if (cal < 0):
+                            cal = cal * -1
+                        if (cal < offset and x > x_left and x < x_right):
+                            print("y:" + str(y))
+                            print("x:" + str(x))
+                            carno += 1
+                            cars.remove((x, y))
+                            print(carno)
 
                 cv2.namedWindow("frame", 0)  # 0可调大小，注意：窗口名必须imshow里面的一窗口名一直
                 cv2.resizeWindow("frame", 1200, 800)  # 设置长和宽
@@ -140,7 +140,7 @@ def cal(cap, x_right, x_left, min_w, min_h, line_high, cars, ambulance_classifie
 
             key = cv2.waitKey(1)
 
-            if (key == 27 or ((time.time()) - start) >= 20.0 ):
+            if (key == 27 or ((time.time()) - start) >= 15.0 ):
                 print(("end:" + str(time.time())))
                 break
 
